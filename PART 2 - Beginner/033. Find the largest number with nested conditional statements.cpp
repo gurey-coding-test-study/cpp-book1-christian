e7892f27bbba9a21345fbe004f64b7c59cf3cf20
SY KOO <34309653+Maydoni@users.noreply.github.com>
2022-07-09 20:19:10 +0900

    Create 033. Find the largest number with nested conditional statements.cpp

---
+++ PART 2 - Beginner/033. Find the largest number with nested conditional statements.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int number1 = 100;
+	int number2 = 200;
+	int number3 = 300;
+
+	if (number1 > number2 && number1 > number3)
+		cout << "가장 큰 수 nunber1 : " << number1 << endl;
+	else if (number2 > number1 && number2 > number3)
+		cout << "가장 큰 수 nunber2 : " << number2 << endl;
+	else if (number3 > number1 && number3 > number2)
+		cout << "가장 큰 수 nunber3 : " << number2 << endl;
+	else
+		cout << "찾을 수 없습니다." << endl;
+
+	return 0;
+}
