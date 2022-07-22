3bf1c638bd3bee9bf256b5540dd1d6ca62aabf24
SY KOO <34309653+Maydoni@users.noreply.github.com>
2022-07-22 22:34:28 +0900

    프로그램 실행 폴더 알아내기(current_path)

---
+++ PART 3 - Intermediate/097. Find out which folder to run the program.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <filesystem>
+
+using namespace std;
+namespace fs = experimental::filesystem;
+
+int main()
+{
+	cout << "프로젝트 폴더 : " << fs::current_path() << endl;
+
+	return 0;
+}
