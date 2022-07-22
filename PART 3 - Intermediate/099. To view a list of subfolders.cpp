2482ac36fac9c0c0c227b67141ac3c8f98e69b4c
SY KOO <34309653+Maydoni@users.noreply.github.com>
2022-07-22 22:38:12 +0900

    하위 폴더 목록 확인하기(directory_iterator)

---
+++ PART 3 - Intermediate/099. To view a list of subfolders.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <filesystem>
+#include <string>
+
+using namespace std;
+namespace fs = experimental::filesystem;
+
+int main()
+{
+	string directory = "C:\\Program Files";
+
+	for (auto& name : fs::directory_iterator(directory))
+		std::cout << name << '\n';
+
+	return 0;
+}
