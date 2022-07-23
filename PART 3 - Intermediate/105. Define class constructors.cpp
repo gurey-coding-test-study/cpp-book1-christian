c16a31e08a07c664fc781dbed6a45fe9659de56f
SY KOO <34309653+Maydoni@users.noreply.github.com>
2022-07-23 22:18:34 +0900

    클래스 생성자 정의하기

---
+++ PART 3 - Intermediate/105. Define class constructors.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class KingInfo
+{
+public:
+	KingInfo()
+	{
+		name_ = "조선 예종 이황";
+	};
+
+public:
+	string GetName() const
+	{
+		return name_;
+	}
+
+private:
+	string name_;
+};
+
+int main()
+{
+	KingInfo king_info;
+
+	cout << king_info.GetName() << endl;
+
+	return 0;
+}
