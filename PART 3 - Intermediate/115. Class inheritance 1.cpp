da397a650bfa87ae5cd5c8b83f211df755d85933
SY KOO <34309653+Maydoni@users.noreply.github.com>
2022-07-25 21:31:22 +0900

    Update 115. Class inheritance 1.cpp

--- PART 3 - Intermediate/115. Class inheritance 1.cpp
+++ PART 3 - Intermediate/115. Class inheritance 1.cpp
@@ -1,1 +1,28 @@
+//#include "stdafx.h"
+#include <iostream>
 
+using namespace std;
+
+class Building1
+{
+public:
+	void Name() { cout << "노른자 위 좋은 건물" << endl; }
+};
+
+class BuildingOwner : public Building1
+{
+public:
+	void MyBuilding()
+	{
+		cout << "내 보물 : ";
+		Name();
+	}
+};
+
+int main(void)
+{
+	BuildingOwner envious;
+	envious.MyBuilding();
+
+	return 0;
+}
